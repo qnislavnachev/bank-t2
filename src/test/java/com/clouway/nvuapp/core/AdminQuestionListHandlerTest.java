7a01593693a26d99d54d8a32ed8888dbbd2d1b3c
qnislavnachev <qnislav.nachev@gmail.com>
2017-01-10 09:37:12 +0200

    created ResponceReader class that is used to avoid byteArrayStreams in tests

--- src/test/java/com/clouway/nvuapp/core/AdminQuestionListHandlerTest.java
+++ src/test/java/com/clouway/nvuapp/core/AdminQuestionListHandlerTest.java
@@ -2,19 +2,16 @@ package com.clouway.nvuapp.core;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
-import com.google.common.io.ByteStreams;
 import core.Question;
 import core.Request;
 import core.Response;
 import http.controllers.AdminQuestionListHandler;
 import org.jmock.integration.junit4.JUnitRuleMockery;
 import org.junit.Test;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
+import static core.ResponseReader.reader;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
@@ -39,11 +36,11 @@ public class AdminQuestionListHandlerTest {
 
         Response response = questionListHandler.handle(request);
 
-        assertThat(toString(response), containsString("User-1234 How are you today?"));
-        assertThat(toString(response), containsString("User-1234 How you feel today?"));
+        assertThat(reader().read(response), containsString("User-1234 How are you today?"));
+        assertThat(reader().read(response), containsString("User-1234 How you feel today?"));
 
-        assertThat(toString(response), containsString("User-0987 How are you today?"));
-        assertThat(toString(response), containsString("User-0987 How you feel today?"));
+        assertThat(reader().read(response), containsString("User-0987 How are you today?"));
+        assertThat(reader().read(response), containsString("User-0987 How you feel today?"));
     }
 
     @Test
@@ -54,7 +51,7 @@ public class AdminQuestionListHandlerTest {
 
         Response response = questionListHandler.handle(request);
 
-        assertThat(toString(response), containsString("Няма добавени въпроси до момента"));
+        assertThat(reader().read(response), containsString("Няма добавени въпроси до момента"));
     }
 
     @Test
@@ -67,16 +64,5 @@ public class AdminQuestionListHandlerTest {
         String uri = response.headers().get("Location");
 
         assertThat(uri, equalTo("/home"));
-    }
-
-    private String toString(Response response) {
-        String page = "";
-        try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
-            ByteStreams.copy(response.body(), out);
-            page = out.toString();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return page;
     }
 }
