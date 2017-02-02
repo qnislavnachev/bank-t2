f6567ba33568544a12455bcced7344ab2238b4b1
qnislavnachev <qnislav.nachev@gmail.com>
2017-02-02 13:01:24 +0200

    nvuApp: added answer to questions
    when admin is logged and open some questionnaires
    he can see the answer of the questions in it

--- src/main/java/com/clouway/nvuapp/adapter/http/controllers/ViewQuestionnaireHandler.java
+++ src/main/java/com/clouway/nvuapp/adapter/http/controllers/ViewQuestionnaireHandler.java
@@ -5,6 +5,7 @@ import com.clouway.nvuapp.adapter.http.servlet.RsRedirect;
 import com.clouway.nvuapp.core.*;
 
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ViewQuestionnaireHandler implements SecuredHandler {
@@ -27,8 +28,19 @@ public class ViewQuestionnaireHandler implements SecuredHandler {
 
     private Map<String, Object> getTemplates(Questionnaire questionnaire, int id) {
         return new LinkedHashMap<String, Object>(){{
-            put("questions", questionnaire.getQuestions());
+            put("questions", getAnsweredQuestions(questionnaire));
             put("num", id);
         }};
+    }
+
+    public Map<String, Question> getAnsweredQuestions(Questionnaire questionnaire) {
+        Map<String, Question> questionMap = new LinkedHashMap<>();
+        Map<Integer, String> answers = questionnaire.getAnswers();
+        List<Question> questions = questionnaire.getQuestions();
+
+        for (int i = 0; i < questions.size(); i++) {
+            questionMap.put(answers.get(i + 1), questions.get(i));
+        }
+        return questionMap;
     }
 }
